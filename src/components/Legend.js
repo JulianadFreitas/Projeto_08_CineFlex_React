620d508442d405a571f82a481d8aee80a55e5547
thiagoribb <thiribeiro142@gmail.com>
2021-05-18 16:36:38 -0300

    Preparando review

--- src/components/Legend.js
+++
@@ -1,9 +0,0 @@
-
-import React from "react";
-export default function Legend({ text, classe}) {
-  return (
-    <div className={classe}>
-      <p>{text}</p>
-    </div>
-  );
-}
--- src/components/Stage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function Stage({text}) {
-  return (
-    <div className='page'>
-        {text}
-    </div>
-
-  );
-}
